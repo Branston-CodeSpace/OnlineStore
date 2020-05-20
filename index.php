8e55296a5eced8e22e5fc97d8c700008a3c2a8d1
Branston <brnstnmrng@gmail.com>
2020-05-20 17:18:13 +0200

    added social buttons

--- index.php
+++ index.php
@@ -172,7 +172,7 @@
 									<input type="hidden" name="cmd" value="_cart">
 									<input type="hidden" name="display" value="1">
 									<button class="btn w3view-cart" type="submit" name="submit" value="">
-										<i class="fas fa-cart-arrow-down"></i>
+										<img src="images/cart.png" alt="" class="img-fluid"></i>
 									</button>
 								</form>
 							</div>
@@ -1200,31 +1200,6 @@
 		<div class="w3l-middlefooter-sec">
 			<div class="container py-md-5 py-sm-4 py-3">
 				<div class="row footer-info w3-agileits-info">
-					<!-- footer categories -->
-					<div class="col-md-3 col-sm-6 footer-grids">
-						<h3 class="text-white font-weight-bold mb-3">Categories</h3>
-						<ul>
-							<li class="mb-3">
-								<a href="product.html">Mobiles </a>
-							</li>
-							<li class="mb-3">
-								<a href="product.html">Computers</a>
-							</li>
-							<li class="mb-3">
-								<a href="product.html">TV, Audio</a>
-							</li>
-							<li class="mb-3">
-								<a href="product2.html">Smartphones</a>
-							</li>
-							<li class="mb-3">
-								<a href="product.html">Washing Machines</a>
-							</li>
-							<li>
-								<a href="product2.html">Refrigerators</a>
-							</li>
-						</ul>
-					</div>
-					<!-- //footer categories -->
 					<!-- quick links -->
 					<div class="col-md-3 col-sm-6 footer-grids mt-sm-0 mt-4">
 						<h3 class="text-white font-weight-bold mb-3">Quick Links</h3>
@@ -1269,16 +1244,6 @@
 						</ul>
 					</div>
 					<div class="col-md-3 col-sm-6 footer-grids w3l-agileits mt-md-0 mt-4">
-						<!-- newsletter -->
-						<h3 class="text-white font-weight-bold mb-3">Newsletter</h3>
-						<p class="mb-3">Free Delivery on your first order!</p>
-						<form action="#" method="post">
-							<div class="form-group">
-								<input type="email" class="form-control" placeholder="Email" name="email" required="">
-								<input type="submit" value="Go">
-							</div>
-						</form>
-						<!-- //newsletter -->
 						<!-- social icons -->
 						<div class="footer-grids  w3l-socialmk mt-3">
 							<h3 class="text-white font-weight-bold mb-3">Follow Us on</h3>
@@ -1286,17 +1251,17 @@
 								<ul>
 									<li>
 										<a class="icon fb" href="#">
-											<i class="fab fa-facebook-f"></i>
+											<img src="images/fb.png" alt="facebook">
 										</a>
 									</li>
 									<li>
 										<a class="icon tw" href="#">
-											<i class="fab fa-twitter"></i>
+											<img src="images/tw.png" alt="twitter">
 										</a>
 									</li>
 									<li>
 										<a class="icon gp" href="#">
-											<i class="fab fa-google-plus-g"></i>
+											<img src="images/ig.png" alt="instagram">
 										</a>
 									</li>
 								</ul>
