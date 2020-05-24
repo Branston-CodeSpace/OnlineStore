2c091506166b0b44476fb9c4214312c4e88414b1
Branston <brnstnmrng@gmail.com>
2020-05-24 12:06:07 +0200

    changed username and password

--- credentials.php
+++ credentials.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_SERVER', 'http://167.71.74.228/phpmyadmin/');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'branston');
+define('DB_PASSWORD', 'atlas77basso');
 define('DB_NAME', 'todoapp');
 
 ?>
