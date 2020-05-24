ad1e6f0334b843362208f0734f684b8856837620
Branston <brnstnmrng@gmail.com>
2020-05-24 12:07:38 +0200

    changed database name

--- credentials.php
+++ credentials.php
@@ -3,6 +3,6 @@
 define('DB_SERVER', 'http://167.71.74.228/phpmyadmin/');
 define('DB_USERNAME', 'branston');
 define('DB_PASSWORD', 'atlas77basso');
-define('DB_NAME', 'todoapp');
+define('DB_NAME', 'WDI2020_branston');
 
 ?>
