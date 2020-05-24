e5a9aad6da950deef6a894bf6e912ca1016cbf5f
Branston <brnstnmrng@gmail.com>
2020-05-24 11:48:06 +0200

    small changes

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+
+session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,20 +21,12 @@
 			window.scrollTo(0, 1);
 		}
 	</script>
-	<!-- //Meta tag Keywords -->
 
-	<!-- Custom-Files -->
 	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-	<!-- Bootstrap css -->
 	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-	<!-- Main css -->
 	<link rel="stylesheet" href="css/fontawesome-all.css">
-	<!-- Font-Awesome-Icons-CSS -->
 	<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
-	<!-- pop-up-box -->
 	<link href="css/menu.css" rel="stylesheet" type="text/css" media="all" />
-	<!-- menu style -->
-	<!-- //Custom-Files -->
 
 	<!-- web fonts -->
 	<link href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
--- login.php
+++ login.php
@@ -2,11 +2,6 @@
 
 session_start();
 
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-  header("location: index.php");
-  exit;
-}
-
 require_once "connect.php";
 
 $username = $password = "";
